6883accda45e690b12f0c60c961b5abf7ec67153
Miguel <miguel.angel2007.gonzalez@gmail.com>
2023-05-01 10:59:42 -0500

    Primer commit, Clase Jugador, Ventanas principales

---
+++ src/Jugador/Jugador.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jugador;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Jugador {
+    
+    //ATRIBUTOS
+    
+    public String nombre1;
+    public String nombre2;
+    public static int categoriaSeleccionada;
+    
+    // METODO CONSTRUCTOR
+    public Jugador(){
+        nombre1 = "NONE";
+        nombre2 = "NONE";
+        categoriaSeleccionada = 0;
+    }
+    
+    public Jugador(String nombre){
+        this.nombre1 = nombre;
+    }
+
+    public String getNombre1() {
+        return nombre1;
+    }
+    
+    public void setNombre1(String nombre) {
+        this.nombre1 = nombre;
+    }
+    
+    public static int get_categoriaSeleccionada() {
+        return categoriaSeleccionada;
+    }
+    
+    public void set_categoriaSeleccionada(int categoriaSeleccionada) {
+        this.categoriaSeleccionada = categoriaSeleccionada;
+    }
+    
+}
---
+++ src/main.java
@@ -0,0 +1,25 @@
+
+import GUI.VentanaPrincipal;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Miguel
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+        ventanaPrincipal.setLocationRelativeTo(null);
+    }
+    
+}
